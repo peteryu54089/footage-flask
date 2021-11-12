ef97fd8b269efcbd4fa9f5ff7315730ca37079a6
peteryu54089 <peteryu54089@gmail.com>
2021-11-12 21:04:11 +0800

    update templates

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('main/index.html')
+
+@app.route('/admin', methods = ['GET', 'POST'])
+def admin():
+    return render_template('main/admin.html')
+
+@app.route('/cms')
+def cms():
+    return render_template('main/cms.html')
