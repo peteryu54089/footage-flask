011cfed5d31913714ca8c8ae0aea6535e155e541
peteryu54089 <peteryu54089@gmail.com>
2021-11-12 23:08:13 +0800

    add sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,27 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from app.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+bcrypt = Bcrypt(app)
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+if not database_exists(engine.url):
+    create_database(engine.url)
+db = SQLAlchemy(app)
+
+from app.models.main import Admin
+
+db.create_all()
+db.session.commit()
+
+if not Admin.query.filter_by(username = 'admin').first():
+    admin = Admin(username = 'admin', password = 'Ty6Lb!Gn85hQ')
+    db.session.add(admin)
+    db.session.commit()
 
 from app.controllers import main
---
+++ app/models/main.py
@@ -0,0 +1,20 @@
+from app import db, bcrypt
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(30), unique = True, nullable = False)
+    password_hash = db.Column(db.String(50), nullable = False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return 'username: %s' % (self.username)
